b700b0949f8980a17fc930a8dadf7e3ade24d422
jogichennaiah <chennaiahjogi394@gmail.com>
2023-11-11 00:31:04 +0530

    Auto push

---
+++ env-prod/prod-backend.tfvars
@@ -0,0 +1,4 @@
+bucket  = "b55-terraform-bucket"
+key     = "alb/prod/terraform.tfstate"
+region  = "us-east-1"
+ 
---
+++ main.tf
@@ -0,0 +1,13 @@
+module "alb-public" {
+  source                    = "./vendor/modules/alb"
+  ENV                       = var.ENV
+  ALB_NAME                  = roboshop-public
+  INTERNAL                  = false
+}
+
+module "alb-private" {
+  source                    = "./vendor/modules/alb"
+  ENV                       = var.ENV
+  ALB_NAME                  = roboshop-private
+  INTERNAL                  = true
+}
