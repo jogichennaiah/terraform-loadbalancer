27600afd6d7293f71208965c0b6d936413522774
jogichennaiah <chennaiahjogi394@gmail.com>
2023-11-13 23:22:35 +0530

    Auto push

--- outputs.tf
+++ outputs.tf
@@ -6,6 +6,15 @@ output "PRIVATE_ALB_ARN" {
     value = module.alb-private.ALB_ARN
 }
 
+output "PUBLIC_ALB_ADDRESS" {
+    value = module.alb-public.ALB_ADDRESS
+}
+
+output "PRIVATE_ALB_ADDRESS" {
+    value = module.alb-private.ALB_ADDRESS
+}
+
+
 # output "VPC_ID" {
 #    value = module.vpc.VPC_ID
 # }
