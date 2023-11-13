f1e477e99d19f39b051c76c58c2784e9f6a79378
jogichennaiah <chennaiahjogi394@gmail.com>
2023-11-13 16:51:32 +0530

    Auto push

--- outputs.tf
+++ outputs.tf
@@ -1,47 +1,55 @@
-#output "VPC_ID" {
+output "PUBLIC_ALB_ARN" {
+    value = module.alb-public.ALB_ARN
+}
+
+output "PRIVATE_ALB_ARN" {
+    value = module.alb-private.ALB_ARN
+}
+
+# output "VPC_ID" {
 #    value = module.vpc.VPC_ID
-#}
+# }
 
-#output "VPC_CIDR" {
+# output "VPC_CIDR" {
 #    value = module.vpc.VPC_CIDR
-#}
+# }
 
-#output "PUBLIC_SUBNET_CIDR" {
+# output "PUBLIC_SUBNET_CIDR" {
 #    value = module.vpc.PUBLIC_SUBNET_CIDR
-#}
+# }
 
-#output "PRIVATE_SUBNET_CIDR" {
+# output "PRIVATE_SUBNET_CIDR" {
 #    value = module.vpc.PRIVATE_SUBNET_CIDR
-#}
+# }
 
-#output "PUBLIC_SUBNET_IDS" {
+# output "PUBLIC_SUBNET_IDS" {
 #    value = module.vpc.PUBLIC_SUBNET_IDS
-#}
+# }
 
-#output "PRIVATE_SUBNET_IDS" {
+# output "PRIVATE_SUBNET_IDS" {
 #    value = module.vpc.PRIVATE_SUBNET_IDS
-#}
+# }
 
-#output "DEFAULT_VPC_ID" {
+# output "DEFAULT_VPC_ID" {
 #    value = module.vpc.DEFAULT_VPC_ID
-#}
+# }
 
-#output "DEFAULT_VPC_CIDR" {
+# output "DEFAULT_VPC_CIDR" {
 #    value = module.vpc.DEFAULT_VPC_CIDR
-#}
+# }
 
-#output "PRIVATE_HOSTED_ZONE_ID" {
+# output "PRIVATE_HOSTED_ZONE_ID" {
 #    value = var.PRIVATE_HOSTED_ZONE_ID
-#}
+# }
 
-#output "PRIVATE_HOSTED_ZONE_NAME" {
+# output "PRIVATE_HOSTED_ZONE_NAME" {
 #    value = var.PRIVATE_HOSTED_ZONE_NAME
-#}
+# }
 
-#output "Public_HOSTED_ZONE_ID"{
+# output "Public_HOSTED_ZONE_ID"{
 #    value = var.PUBLIC_HOSTED_ZONE_ID
-#}
+# }
 
-#output "PUBLUC_HOSTED_ZONE_NAME" {
+# output "PUBLUC_HOSTED_ZONE_NAME" {
 #    value = var.PUBLIC_HOSTED_ZONE_NAME
-#}
+# }
