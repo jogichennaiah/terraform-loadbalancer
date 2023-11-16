27a1aaae90b49455f7ba9612682b614e05c840df
jogichennaiah <chennaiahjogi394@gmail.com>
2023-11-16 22:47:10 +0530

    Auto push

--- main.tf
+++ main.tf
@@ -3,6 +3,7 @@ module "alb-public" {
   ENV                       = var.ENV
   ALB_NAME                  = "roboshop-public"
   INTERNAL                  = false
+  APP_PORT                  = 8080
 }
 
 module "alb-private" {
@@ -10,4 +11,5 @@ module "alb-private" {
   ENV                       = var.ENV
   ALB_NAME                  = "roboshop-private"
   INTERNAL                  = true
+  APP_PORT                  = 8080
 }
